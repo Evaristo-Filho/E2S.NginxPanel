04c45ee0ab2c613660e447291f041bc8e4c3affe
Evaristo Filho <evaristossfilho@hotmail.com>
2026-02-25 14:57:45 -0300

    UI initial commit

---
+++ Source/E2S.NginxPanel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace E2S.NginxPanel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/E2S.NginxPanel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+
+namespace E2S.NginxPanel
+{
+    
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
