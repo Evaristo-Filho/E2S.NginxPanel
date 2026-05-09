410503942d2d2c1f019e77be88ca23e1f4a1735b
Evaristo Filho <evaristossfilho@hotmail.com>
2026-05-09 14:48:14 -0300

    Added android support

---
+++ Source/E2S.NginxPanel/Platforms/Android/MainApplication.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Runtime;
+using E2S.NginxPanel;
+
+namespace E2S.NginxPanel.Platforms.Android
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Source/E2S.NginxPanel/ViewModel/BaseVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace E2S.NginxPanel.ViewModel
+{
+    public abstract class BaseVm
+    {
+        public event ChangeLabelsHandler OnLanguageChange;
+        public event EventHandler GlobalEvent;
+        public delegate void ChangeLabelsHandler(IReadOnlyList<IVisualTreeElement> t);
+        //public delegate void Global(Evend);
+
+        public BaseVm()
+        {
+            //var ff = new EventHandler();
+            
+            //OnLanguageChange?.Invoke("PT-BR");
+        }
+
+        public abstract void ChangeLabels(IReadOnlyList<IVisualTreeElement> name);
+
+    }
+}
