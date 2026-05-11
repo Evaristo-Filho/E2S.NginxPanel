663b5245fbf5a9e168e8a2625a7cf3fb5cec6cb3
Evaristo Filho <evaristossfilho@hotmail.com>
2026-05-11 19:36:13 -0300

    Fix to selective build. Preprocessor directives bugged on  vs2026

--- Source/E2S.NginxPanel/MauiProgram.cs
+++ Source/E2S.NginxPanel/MauiProgram.cs
@@ -5,20 +5,24 @@ namespace E2S.NginxPanel
 {
     public static class MauiProgram
     {
-        
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
+#if WINDOWS
             builder
-                .UseMauiApp<App>().UseMauiCommunityToolkitMarkup()
+                .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
+#elif ANDROID
+            builder.UseMauiApp<App>().UseMauiCommunityToolkitMarkup();
+#endif
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
